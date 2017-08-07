87204df6543f53d400e46ff4ee147371e62a2c0d
Ryan Bregier <rabunc@gmail.com>
2017-08-06 20:57:59 -0400

    updated ClozeCard

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,16 +1,12 @@
 module.exports = ClozeCard;
 
 function ClozeCard(text, cloze) {
-    this.cloze = cloze;
-    this.fullText = text;
-    this.partial = function () {
-        // if the cloze is in the text
-        if (text.indexOf(cloze) >= 0) {
-            var partialText = text.replace(cloze, '...');
-            return partialText
-        } else {
-            // log an error
-            console.log("Error! Cloze is not in text!")
-        }
+    if (text.indexOf(cloze) >= 0) {
+        this.partial = text.replace(cloze, '...');
+        this.cloze = cloze;
+        this.fullText = text;
+    } else {
+        // log an error
+        console.log("Error! Cloze is not in text!")
     }
 }
